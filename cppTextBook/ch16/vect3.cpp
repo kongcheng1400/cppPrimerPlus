24e719d8c8165197f49101f6d099658e443886b5
kongcheng1400 <he.weiwei@broadcom.com>
2018-07-30 20:38:17 -0400

    20180731

--- cppTextBook/ch16/vect3.cpp
+++ cppTextBook/ch16/vect3.cpp
@@ -33,7 +33,97 @@ int main()
 
 		sort(books.begin(), books.end());
 	    cout << "sorted by title:\nRating\tBook\n";	
+		for_each(books.begin(), books.end(), showReview);
+
+		random_shuffle(books.begin(), books.end());
+		cout << "after shuffling:\nRating\tBook\n";
+		for_each(books.begin(), books.end(), showReview);
 	}
+	else
+		cout << "No entries.";
+
+	cout << "bye.\n";
+	return 0;
 
 
 }
+
+bool operator<(const Review & r1, const Review & r2)
+{
+	if (r1.title < r2.title)
+		return true;
+	else if (r1.rating < r2.rating)
+		return true;
+	else
+		return false;
+
+}
+
+
+bool worseThan(const Review & r1, const Review & r2)
+{
+	if (r1.rating < r2.rating)
+		return true;
+	else
+		return false;
+}
+
+bool fillReview(Review & rr)
+{
+	using namespace std;
+	cout << "Please enter the book name and raint, quit for quit\n";
+	cout << "book name: ";
+	getline(cin, rr.title);
+	if (rr.title == "quit")
+		return false;
+	
+	cout << "book rating: ";
+	if (!(cin >> rr.rating))
+		return false;
+	
+	while (cin.get() != '\n')
+		continue;
+	return true;
+}
+
+
+void showReview(const Review & rr)
+{
+	std::cout << rr.rating << '\t' << rr.title << std::endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
